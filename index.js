e4bd41e3a484bbd954bdf92bff00f4d57d11dba7
valentina-RGB <valensc2545@gmail.com>
2025-05-16 11:49:18 -0500

    feat: create productList object with name and price inputs

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const products = []
 const form = document.getElementById("product-form")
 const nameInput = document.getElementById("name-input")
 const priceInput = document.getElementById("price-input")
+
+
+const productList = {
+    name: nameInput,
+    price: priceInput
+}
+
+
+console.log(productList)
