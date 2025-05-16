efd7e243014db47eb901d20ddeaf24a5921ba17e
juan pablo gomez <juan_pgomez16@soy.sena.edu.co>
2025-05-16 12:29:11 -0500

    feat: agregar funcionalidad para guardar productos en la lista

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ const nameInput = document.getElementById("name-input")
 const priceInput = document.getElementById("price-input")
 
 document.getElementById("product-form").addEventListener("submit", function(event){
-    
+    event.preventDefault()
+    let list= {
+       name: nameInput.value,
+       price: priceInput.value,
+    }
+    products.push(list)
+const producList = document.getElementById ("product_list")
 })
