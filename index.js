516333d5b4890a309b911482fcafd6bebf82fd81
Santiago Herrera <santiherreravelas@gmail.com>
2025-05-16 11:39:03 -0500

    feat: adding form

---
+++ index.js
@@ -0,0 +1,4 @@
+const products = []
+const form = document.getElementById("product-form")
+const nameInput = document.getElementById("name-input")
+const priceInput = document.getElementById("price-input")
